672d0cad1566b7a7e8b8ab1b8824f1f36ff7d754
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-11-02 23:23:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port =process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+// name:emaJohnDB
+// password:xntoZJCzGPImtX3E
+
+// database connection
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ruqflxh.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+
+    const productCollection = client.db('emaJohn').collection('products')
+    app.get('/products',async(req,res)=>{
+      const query = {}
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    })
+
+  }
+  finally{
+
+  }
+}
+run().catch(err=>console.error(err))
+
+
+app.get('/',(req,res)=>{
+  res.send('Server is Running')
+})
+
+app.listen(port,()=>{
+  console.log(`Listening to the ${port}`)
+})
